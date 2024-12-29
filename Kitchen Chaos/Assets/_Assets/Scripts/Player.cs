964c538b0f4fe64cde9b2271fa5e540e16245657
Nathaniel Murphy <74879919+Hey-itsnate@users.noreply.github.com>
2024-12-29 02:27:24 -0600

    Added CounterSelectedVisual Logic && Modified Player Move animation to only activate when player  can move

--- Kitchen Chaos/Assets/_Assets/Scripts/Player.cs
+++ Kitchen Chaos/Assets/_Assets/Scripts/Player.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneTemplate;
 using UnityEngine;
 
 
 public class Player : MonoBehaviour
 {
-    #region
+    
+
+    #region Fields
+
+    //Events
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs 
+    {
+        public ClearCounter SelectedCounter;
+    }
+
     [SerializeField] private float MoveSpeed = 2f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask counterLayerMask;
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
 
     #endregion
+
+    #region Properties
+
+    public static Player Instance { get; private set; }
+
+    #endregion
+
+    #region Methods
+
+    #region Unity Methods
+
+    private void Awake()
+    {
+        if (Instance != null) 
+        {
+            Debug.LogError("There is moire than one Player Instance.");
+            return;
+        }
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,35 +53,33 @@ public class Player : MonoBehaviour
         gameInput.OnInteractPeformed += GameInput_OnInteractPeformed;
     }
 
-    private void GameInput_OnInteractPeformed(object sender, System.EventArgs e)
-    {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero)
-        {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 2f;
-        RaycastHit hit;
-        //Out sets the given parameter to a value based on the function
-        if (Physics.Raycast(transform.position, moveDir, out hit, interactDistance, counterLayerMask))
-        {
-            if (hit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                clearCounter.Interact();
-            }
-
-        }
-
-    }
-
     // Update is called once per frame
     private void Update()
     {
         HandleMovement();
         HandleInteractions();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <returns>True if player is walking.</returns>
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void GameInput_OnInteractPeformed(object sender, System.EventArgs e)
+    {
+        if (selectedCounter != null) 
+        {
+            selectedCounter.Interact();
+        }
     }
 
     void HandleInteractions() 
@@ -64,24 +95,37 @@ public class Player : MonoBehaviour
         float interactDistance = 2f;
         RaycastHit hit;
         //Out sets the given parameter to a value based on the function
-        if (Physics.Raycast(transform.position, moveDir, out hit, interactDistance, counterLayerMask)) 
+        if (Physics.Raycast(transform.position, moveDir, out hit, interactDistance, counterLayerMask))
         {
-            if (hit.transform.TryGetComponent(out ClearCounter clearCounter)) 
+            //Check if RayCast hit is a clear counter
+            if (hit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-
+                //Has Clear Counter
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
             }
-                
-        }    
-
+            else 
+            {
+                //RayCast Hit isn't a counter
+                SetSelectedCounter(null);
+            }
+        }
+        else 
+        {
+            //RayCat Hit is null
+            SetSelectedCounter(null);
+        }
     }
 
     void HandleMovement() 
     {
-        //Move Player
+        //Get Movement Input
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        isWalking = (moveDir != Vector3.zero);
-        //Check if player should move 
+
+        //Check if player can move 
         float moveDist = MoveSpeed * Time.deltaTime;
         float playerSize = .7f;
         float playerHeight = 2f;
@@ -89,11 +133,9 @@ public class Player : MonoBehaviour
         if (!canMove)
         {
             //Cannot Move towards moveDir
-
             //Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirX, moveDist);
-
             if (canMove)
             {
                 //Can move only on the x
@@ -111,23 +153,28 @@ public class Player : MonoBehaviour
                     moveDir = moveDirZ;
                 }
                 else
-                { //Can't move at all
+                { 
+                    //Can't move at all
                 }
             }
         }
 
-        if (canMove)
-            transform.position += moveDir * MoveSpeed * Time.deltaTime;
-
-
+        //Move Player
+        if (canMove) transform.position += moveDir * MoveSpeed * Time.deltaTime;
+        isWalking = (moveDir != Vector3.zero && canMove);
 
         //Rotate Player towards direction
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 
-    public bool IsWalking() 
+    private void SetSelectedCounter(ClearCounter selectedCounter) 
     {
-        return isWalking;
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs { SelectedCounter = selectedCounter }); 
     }
+
+    #endregion
+
+    #endregion
 }
