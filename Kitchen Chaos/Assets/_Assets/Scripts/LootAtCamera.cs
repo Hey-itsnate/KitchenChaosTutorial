dddcb18967d38e70449d0129b2cc57629e0c9a91
Nathaniel Murphy <74879919+Hey-itsnate@users.noreply.github.com>
2024-12-30 05:13:02 -0600

    LootAt Camera Script w/ four modes

---
+++ Kitchen Chaos/Assets/_Assets/Scripts/LootAtCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LootAtCamera : MonoBehaviour
+{
+    private enum LookMode { LookAt, LookAtInverted,CameraFoward, CameraForwardInverted}
+
+    [SerializeField] LookMode lookMode;
+
+    private void LateUpdate()
+    {
+        switch (lookMode) 
+        {
+            case LookMode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case LookMode.LookAtInverted:
+                Vector3 DirToCam =  transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + DirToCam); 
+                break;
+            case LookMode.CameraFoward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case LookMode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
